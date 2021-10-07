f28a0851940c4bfff882070910f789b6ad8d3d0f
Henrik Näkne <henrik_nakne@discovery.com>
2021-10-07 11:11:36 +0200

    fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,8 @@ object Dependencies {
   val SparkSql = "org.apache.spark" %% "spark-sql" % Version.Spark
   val SparkDaria = "com.github.mrpowers" %% "spark-daria" % "0.38.2"
   val SparkFastTests = "com.github.mrpowers" %% "spark-fast-tests" % "0.21.3"
-  val ScalaTests = "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+  val ScalaTests = "org.scalatest" %% "scalatest" % "3.0.1"
+
 
   object Version {
     val Aws = "1.11.243"
